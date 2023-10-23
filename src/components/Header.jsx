ddccdb39b2732fdb8b2d9313d08961d9923fb6c5
nzmort <118791096+nozomuorita@users.noreply.github.com>
2023-10-24 06:42:42 +0900

    hoge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,53 @@ import audiosource from "../../public/audio.mp3";
 
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
 //const music = new Audio('../src/assets/TheFatRat - Windfall (online-audio-converter.com).mp3');
 // const music = new Audio("../src/assets/audio.mp3");
 const music = new Audio(audiosource);
